b6eebd4c349d5ab99ef7d43d89654a115befa86d
Muhammad Ilham Hafizha <ilham.hafizh7@gmail.com>
2022-10-01 14:23:59 +0700

    add library gradle

---
+++ app/src/main/java/com/example/vix_schoters_muhammadilhamhafizha/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vix_schoters_muhammadilhamhafizha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
